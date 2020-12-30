a15c6f211f32abecdee84af57fa6c7c4ff9f1415
sushantbasak <sushantbasak73@gmail.com>
2020-12-31 00:25:15 +0530

    Repo Initialized

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './Container/Home/Home';
+import About from './Container/About/About';
+
+function App() {
+  return (
+    <>
+        <div>
+          Hello World
+        </div>
+    </>
+  );
+}
+
+export default App;
