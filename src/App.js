c94c46c32161748db5b02c94f6ad400018073607
sushantbasak <sushantbasak73@gmail.com>
2021-01-06 10:25:34 +0530

    Router Configured and Nav Updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import Home from "./Container/Home/Home";
 import About from "./Container/About/About";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import Nav from "./Container/Nav/Nav";
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <div>
-          <ul>
-            <li>
-              <Link to="/"> Home </Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-        </Switch>
-      </div>
-    </Router>
+    <>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </>
   );
 }
