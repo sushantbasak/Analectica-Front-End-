f87f79d07c0ad265b5562b4487060a818115c121
sushantbasak <sushantbasak73@gmail.com>
2021-01-16 09:42:13 +0530

    Navbar Bootstrapped

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import Home from "./Container/Home/Home";
-import About from "./Container/About/About";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
 import { Switch, Route } from "react-router-dom";
 import Nav from "./Container/Nav/Nav";
 
@@ -10,6 +12,8 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
+        <Route path="/users/login" component = {Login} />
+        <Route path="/users/register" component = {Register} />
       </Switch>
     </>
   );
