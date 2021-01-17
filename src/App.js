dbb1b75fc51552307dffe8c780355f74d0e3aef3
sushantbasak <sushantbasak73@gmail.com>
2021-01-17 18:42:29 +0530

    Login form bootstrapped

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
+import NotFound from "./Pages/Not Found/NotFound";
 import { Switch, Route } from "react-router-dom";
 import Nav from "./Container/Nav/Nav";
 
@@ -12,8 +13,9 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
-        <Route path="/users/login" component = {Login} />
-        <Route path="/users/register" component = {Register} />
+        <Route path="/login" component = {Login} />
+        <Route path="/register" component = {Register} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </>
   );
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,107 @@
-import React from 'react'
+import React ,{useState} from 'react';
+import classes from './Login.module.css';
 
 export default function Login() {
+    const [formData,formUpdate] = useState({email : "", password : ""});
+    const [error,errorChange] = useState([]);
+
+    const handleValidation = () => {
+        let validform = true;
+        let errors = [];
+        
+        //Email
+        if(!formData["email"]){
+            validform = false;
+            errors.push("Email must not be empty")
+         }
+   
+         else if(typeof formData["email"] !== "undefined"){
+            let lastAtPos = formData["email"].lastIndexOf('@');
+            let lastDotPos = formData["email"].lastIndexOf('.');
+
+            if (!(lastAtPos < lastDotPos && lastAtPos > 0 && formData["email"].indexOf('@@') == -1 && lastDotPos > 2 && (formData["email"].length - lastDotPos) > 2)) {
+               validform = false;
+               errors.push("Not a valid email")
+             }
+        }  
+
+         //Password
+         if(!formData["password"]){
+            validform = false;
+            errors.push("Password cannot be empty");
+         }
+         else if(formData["password"].length < 6){
+             validform = false;
+             errors.push("Invalid Password length");
+         }
+        
+        errorChange(errors)
+        
+        return validform;
+    }
+
+    const handleSubmit = (event) => {
+        console.log("fs")
+        event.preventDefault();
+       let p= handleValidation();
+        if(p){
+            alert("Form submitted successfully");
+        }
+        else{
+            let count=0;
+
+            alert(
+                "Errors are as follows \n"+ 
+                error.map(data => {
+                    count++;
+                    return (count + " " + data + "\n") ;
+                })
+            )
+        }
+    }
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        formUpdate(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
     return (
-        <div>
-            Login Page reached
+        <div className={classes.big}>
+            <form className={classes.Container} onSubmit={handleSubmit}>
+                {/* <div>{formData.email}</div> */}
+                <input 
+                type="email"
+                name="email"
+                value={formData.email} 
+                onChange={handleChange}
+                placeholder="Email" 
+                className={classes.input} 
+                // required
+                />
+
+                <input 
+                type="password" 
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="Password" 
+                className={classes.input} 
+                // required
+                />
+
+                <button  
+                type="submit"
+                className={classes.input}>
+                    LogIn
+                </button>
+
+                <button  
+                className={classes.input}>
+                    SignUp
+                </button>
+            </form>
         </div>
     )
 }
