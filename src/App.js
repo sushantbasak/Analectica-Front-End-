8f8f12f970114b034b5afc8efb1e0f4c28b2a6b3
sushantbasak <sushantbasak73@gmail.com>
2021-01-23 13:41:11 +0530

    Clean up unusual files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Login from './Pages/Login/Login';
-import Register from './Pages/Register/Register';
 import NotFound from './Pages/Not Found/NotFound';
 import Nav from './Container/Nav/Nav';
 
@@ -14,7 +13,6 @@ export default function App() {
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
         <Route path="*" component={NotFound} />
       </Switch>
     </>
--- src/Pages/Register/Register.js
+++
@@ -1,5 +0,0 @@
-// import React from 'react';
-// import { useForm } from 'react-hook-form';
-// import classes from './Register.module.css';
-
-// export default Login;
